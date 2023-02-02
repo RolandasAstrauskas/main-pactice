e801b855a4164dfa31a7c4a0d4444a5e81712786
Rolandas Astrauskas <astrauskas.rolandas@gmail.com>
2023-02-02 21:06:34 +0200

    Add-model

---
+++ projects/WebPageTest/BookWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BookWeb.Models
+{
+    public class Category
+    {
+        private int Id { get; set; }
+        public string? Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
